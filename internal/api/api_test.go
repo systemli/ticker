20a8267c632825e9ba2698be494dba4466b2495a
louis <louis@systemli.org>
2018-05-19 21:26:53 +0200

    add testcase for api.go

---
+++ internal/api/api_test.go
@@ -0,0 +1,61 @@
+package api_test
+
+import (
+	"testing"
+	"net/http"
+	"git.codecoop.org/systemli/ticker/internal/api"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDomainEmptyOrigin(t *testing.T) {
+	req := http.Request{}
+
+	c := gin.Context{Request: &req,}
+
+	domain, err := api.GetDomain(&c)
+	assert.Equal(t, "", domain)
+	assert.Equal(t, nil, err)
+}
+
+func TestGetDomainLocalhost(t *testing.T) {
+	req := http.Request{
+		Header: http.Header{
+			"Origin": []string{"http://localhost/"},
+		},
+	}
+
+	c := gin.Context{Request: &req,}
+
+	domain, err := api.GetDomain(&c)
+	assert.Equal(t, "localhost", domain)
+	assert.Equal(t, nil, err)
+}
+
+func TestGetDomainLocalhostPort(t *testing.T) {
+	req := http.Request{
+		Header: http.Header{
+			"Origin": []string{"http://localhost:3000/"},
+		},
+	}
+
+	c := gin.Context{Request: &req,}
+
+	domain, err := api.GetDomain(&c)
+	assert.Equal(t, "localhost", domain)
+	assert.Equal(t, nil, err)
+}
+
+func TestGetDomainWWW(t *testing.T) {
+	req := http.Request{
+		Header: http.Header{
+			"Origin": []string{"http://www.demoticker.org/"},
+		},
+	}
+
+	c := gin.Context{Request: &req,}
+
+	domain, err := api.GetDomain(&c)
+	assert.Equal(t, "demoticker.org", domain)
+	assert.Equal(t, nil, err)
+}
