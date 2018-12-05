0692d6e521c40196e0535032cfd263266570b25b
louis <louis@systemli.org>
2018-12-05 20:04:40 +0100

    add graceful shutdown support

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/sethvargo/go-password/password"
 
 	log "github.com/sirupsen/logrus"
@@ -13,8 +19,31 @@ import (
 )
 
 func main() {
-	api := API()
-	api.Run(Config.Listen)
+	router := API()
+	server := &http.Server{
+		Addr:    Config.Listen,
+		Handler: router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Ticker")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func init() {
