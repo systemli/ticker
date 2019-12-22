b33ac1c94f5ffe3d67f33acc7ffaef56d80f5525
louis <louis@systemli.org>
2019-12-22 15:05:20 +0100

    add message response test with attachments (#2)

--- internal/model/message_test.go
+++ internal/model/message_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/systemli/ticker/internal/model"
@@ -23,7 +24,14 @@ func TestPrepareTweet(t *testing.T) {
 }
 
 func TestNewMessageResponse(t *testing.T) {
+	model.Config = model.NewConfig()
+
 	m := model.NewMessage()
+	m.Attachments = []model.Attachment{{
+		UUID:        uuid.New().String(),
+		Extension:   "jpg",
+		ContentType: "image/jpeg",
+	}}
 	r := model.NewMessageResponse(*m)
 
 	assert.Equal(t, 0, r.ID)
@@ -31,6 +39,8 @@ func TestNewMessageResponse(t *testing.T) {
 	assert.Equal(t, 0, r.Ticker)
 	assert.Equal(t, "", r.TweetID)
 	assert.Equal(t, "", r.TweetUser)
+	assert.Equal(t, 1, len(r.Attachments))
+	assert.Equal(t, "image/jpeg", r.Attachments[0].ContentType)
 	assert.Equal(t, `{"type":"FeatureCollection","features":[]}`, r.GeoInformation)
 }
 
