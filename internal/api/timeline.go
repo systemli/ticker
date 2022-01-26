8085caf442f5befbc811663b1efb2bd5ee0a01ee
louis <louis@systemli.org>
2022-01-26 22:43:22 +0100

    :memo: Add origin parameter to timeline api

--- internal/api/timeline.go
+++ internal/api/timeline.go
@@ -22,10 +22,13 @@ type timelineData struct {
 // GetTimelineHandler returns the public timeline for a ticker.
 // @Summary      Fetch the messages for a ticker.
 // @Description  Endpoint to retrieve the messages from a ticker. The endpoint has a pagination to fetch newer or older
-// @Description  messages.
+// @Description  messages. It is mandatory that the browser sends the origin as a header. This can be overwritten with
+// @Description  a query parameter.
 // @Tags         public
 // @Accept       json
 // @Produce      json
+// @Param        origin  query     string  false  "Origin from the ticker, e.g. demoticker.org"
+// @Param        origin  header    string  false  "Origin from the ticker, e.g. http://demoticker.org"
 // @Param        limit   query     int  false  "Limit for fetched messages, default: 10"
 // @Param        before  query     int  false  "ID of the message we look for older entries"
 // @Param        after   query     int  false  "ID of the message we look for newer entries"
