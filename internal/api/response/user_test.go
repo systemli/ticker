968407fab6bf8fa14d383338334e704b9821d105
louis <louis@systemli.org>
2023-10-08 21:41:13 +0200

    Extend Test Cases

---
+++ internal/api/response/user_test.go
@@ -0,0 +1,27 @@
+package response
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/systemli/ticker/internal/storage"
+)
+
+func TestUsersResponse(t *testing.T) {
+	users := []storage.User{
+		{
+			ID:           1,
+			CreatedAt:    time.Now(),
+			Email:        "user@systemli.org",
+			IsSuperAdmin: true,
+		},
+	}
+
+	usersResponse := UsersResponse(users)
+	assert.Equal(t, 1, len(usersResponse))
+	assert.Equal(t, users[0].ID, usersResponse[0].ID)
+	assert.Equal(t, users[0].CreatedAt, usersResponse[0].CreationDate)
+	assert.Equal(t, users[0].Email, usersResponse[0].Email)
+	assert.Equal(t, users[0].IsSuperAdmin, usersResponse[0].IsSuperAdmin)
+}
