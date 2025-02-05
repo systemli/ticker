12fcec0c5922e3e8a069e910944554104efec4dd
louis <louis@systemli.org>
2025-02-05 11:40:32 +0100

    🐛 Include Fragment to ExtractURL

--- internal/util/url.go
+++ internal/util/url.go
@@ -4,6 +4,6 @@ import "regexp"
 
 // ExtractURLs extracts URLs from a text.
 func ExtractURLs(text string) []string {
-	urlRegex := regexp.MustCompile(`(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)`)
+	urlRegex := regexp.MustCompile(`(https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(),]|%[0-9a-fA-F][0-9a-fA-F]|#)+)`)
 	return urlRegex.FindAllString(text, -1)
 }
