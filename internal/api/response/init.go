d4c19ec4ff6b4d75a5f60fb224aa07fdc533d45e
louis <louis@systemli.org>
2025-02-09 11:28:19 +0100

    🐛 Add missing information to response

--- internal/api/response/init.go
+++ internal/api/response/init.go
@@ -15,14 +15,16 @@ type InitTicker struct {
 }
 
 type InitTickerInformation struct {
-	Author   string `json:"author"`
-	URL      string `json:"url"`
-	Email    string `json:"email"`
-	Twitter  string `json:"twitter"`
-	Facebook string `json:"facebook"`
-	Telegram string `json:"telegram"`
-	Mastodon string `json:"mastodon"`
-	Bluesky  string `json:"bluesky"`
+	Author    string `json:"author"`
+	URL       string `json:"url"`
+	Email     string `json:"email"`
+	Twitter   string `json:"twitter"`
+	Facebook  string `json:"facebook"`
+	Instagram string `json:"instagram"`
+	Threads   string `json:"threads"`
+	Telegram  string `json:"telegram"`
+	Mastodon  string `json:"mastodon"`
+	Bluesky   string `json:"bluesky"`
 }
 
 func InitTickerResponse(ticker storage.Ticker) InitTicker {
@@ -32,14 +34,16 @@ func InitTickerResponse(ticker storage.Ticker) InitTicker {
 		Title:       ticker.Title,
 		Description: ticker.Description,
 		Information: InitTickerInformation{
-			Author:   ticker.Information.Author,
-			URL:      ticker.Information.URL,
-			Email:    ticker.Information.Email,
-			Twitter:  ticker.Information.Twitter,
-			Facebook: ticker.Information.Facebook,
-			Telegram: ticker.Information.Telegram,
-			Mastodon: ticker.Information.Mastodon,
-			Bluesky:  ticker.Information.Bluesky,
+			Author:    ticker.Information.Author,
+			URL:       ticker.Information.URL,
+			Email:     ticker.Information.Email,
+			Twitter:   ticker.Information.Twitter,
+			Facebook:  ticker.Information.Facebook,
+			Instagram: ticker.Information.Instagram,
+			Threads:   ticker.Information.Threads,
+			Telegram:  ticker.Information.Telegram,
+			Mastodon:  ticker.Information.Mastodon,
+			Bluesky:   ticker.Information.Bluesky,
 		},
 	}
 }
